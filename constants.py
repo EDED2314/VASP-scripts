43d37ec7c9d4b6331e0358f325970bdd81d7cb8f
Eddie Tang <91804938+EDED2314@users.noreply.github.com>
2024-08-01 11:04:21 -0400

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,16 @@
+class bcolors:
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+
+OUTPUT_DIR = "POSTOUTPUT"
+NAME_LABEL = "Orientation/Location Molecule Takes"
+ENERGY_LABEL = "Adsorption Energy (eV)"
+
